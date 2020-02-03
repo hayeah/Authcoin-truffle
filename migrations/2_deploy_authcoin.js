ba19673967dd132d94b0094c0098733f3f3d3a69
Howard Yeh <hayeah@gmail.com>
2020-02-03 18:30:54 +0800

    tweaks to make it deploy

--- migrations/2_deploy_authcoin.js
+++ migrations/2_deploy_authcoin.js
@@ -1,8 +1,8 @@
 var AuthCoinContract = artifacts.require("./AuthCoin.sol");
 var BytesUtils = artifacts.require("./utils/BytesUtils.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy([BytesUtils]);
+module.exports = function (deployer) {
+  deployer.deploy(BytesUtils);
   deployer.link(BytesUtils, AuthCoinContract);
-  deployer.deploy([AuthCoinContract]);
+  deployer.deploy(AuthCoinContract);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   networks: {
     development: {
-      gas:8000000,
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      host: '127.0.0.1',
+      port: 23889, // janus QTUM-ETH RPC bridge
+      network_id: '*', // eslint-disable-line camelcase
+      from: '0x7926223070547d2d15b2ef5e7383e541c338ffe9',
+      gasPrice: '0x64', // minimal gas for qtum
     },
     live: {
       // provider: Web3.providers.HttpProvider("http://<host>:<port>"),
@@ -12,8 +13,8 @@ module.exports = {
       host: "127.0.0.1",
       port: 23889,
       // optional config values:
-      gas:200000,
-      gasPrice:0x28,
+      gas: 200000,
+      gasPrice: 0x28,
       from: "0x982fce9ecd065e3b29e0229d8cb2d31dd64a7666"
       // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
       //          - function that returns a web3 provider instance (see below.)
@@ -21,6 +22,14 @@ module.exports = {
       // skipDryRun: - true if you don't want to test run the migration locally before the actual migration (default is false)
       // timeoutBlocks: - if a transaction is not mined, keep waiting for this number of blocks (default is 50)
     }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.17",
+      settings: {
+        evmVersion: "constantinople"
+      }
+    },
   },
   solc: {
     optimizer: {
